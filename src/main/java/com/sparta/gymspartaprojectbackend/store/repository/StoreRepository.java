10af0b247550c3819f2b122db43d477321b1e8dc
Jaeseok Lee <wotjr4017@gmail.com>
2024-08-19 20:12:04 +0900

    🐛 fix: 배포 수정

--- src/main/java/com/sparta/gymspartaprojectbackend/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/gymspartaprojectbackend/store/repository/StoreRepository.java
@@ -13,12 +13,6 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
 
   List<Store> findAllByOwnerAccountId(String accountId);
 
-  @Query("SELECT s FROM Store s WHERE (6371 * acos(cos(radians(:latitude)) * cos(radians(s.latitude)) * cos(radians(s.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(s.latitude)))) < :radius AND (s.storeName LIKE %:keyword% OR s.address LIKE %:keyword%)")
-  List<Store> findByLocationAndKeyword(@Param("latitude") Double latitude, @Param("longitude") Double longitude, @Param("keyword") String keyword, @Param("radius") Double radius);
-
-  @Query("SELECT s FROM Store s WHERE (6371 * acos(cos(radians(:latitude)) * cos(radians(s.latitude)) * cos(radians(s.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(s.latitude)))) < :radius")
-  List<Store> findByLocation(@Param("latitude") Double latitude, @Param("longitude") Double longitude, @Param("radius") Double radius);
-
   @Query("SELECT s FROM Store s WHERE s.storeName LIKE %:keyword% OR s.address LIKE %:keyword%")
   List<Store> findByKeyword(@Param("keyword") String keyword);
 }
